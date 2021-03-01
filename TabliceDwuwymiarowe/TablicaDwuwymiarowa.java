ad9a302d230cc240b5b4e44b7b4801463544eb82
NorbertFicner <77607718+NorbertFicner@users.noreply.github.com>
2021-03-01 09:25:10 +0100

    Wyswietlanie tablicy dwuwymiarowej

--- TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
+++ TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
@@ -7,6 +7,21 @@ public class TablicaDwuwymiarowa {
         for(int i=0;i<tab.length;i++){
             System.out.println(tab[i]);
         }
+        System.out.println("===Deklaracja i dlugosc tablicy===");
         int[][] tab2d =  new int[5][2];
+        tab2d[0][0] = 20;
+        tab2d[0][1] = 50;
+        tab2d[2][1] = -200;
+        tab2d[4][0] = 444;
+        System.out.println(tab2d.length); // dlugosc tablicy dwuwymiarowej?? Nie
+        System.out.println(tab2d.length*tab2d[0].length); // ile elementow ma tablica w ogóle
+        System.out.println("==Wyswietlanie tablicy dwuwymiarowej===");
+        for (int i =0; i<tab2d.length; i++){
+           for(int j =0; j<tab2d[i].length;j++){
+               System.out.println(tab2d[i][j]);
+           }
+            System.out.println("Koniec petli w petli ze zmienna j");
+        }
+        System.out.println("Koniec petli w petli ze zmienna i");
     }
 }
