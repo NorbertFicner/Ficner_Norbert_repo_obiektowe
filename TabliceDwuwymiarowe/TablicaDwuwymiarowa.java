a56ab2cba76b87a25c8cb237fe462f86b06ee64c
NorbertFicner <77607718+NorbertFicner@users.noreply.github.com>
2021-03-01 09:10:26 +0100

    Deklaracja tablicy dwuwymiarowej

--- TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
+++ TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
@@ -3,6 +3,10 @@ public class TablicaDwuwymiarowa {
         int[] tab = new int[5];
         tab[0] = 40;
         //tab[1] = "Jeden";
-
+        // wyskoczy error W int nie moze byc string
+        for(int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
+        }
+        int[][] tab2d =  new int[5][2];
     }
 }
