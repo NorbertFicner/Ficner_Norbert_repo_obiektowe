79e61e876e8b4e075d62609f16c8a9e1855f932b
NorbertFicner <77607718+NorbertFicner@users.noreply.github.com>
2021-03-02 10:01:13 +0100

    Zadanie 02 03 2021

--- Zadanie 02 03 2021/Zadanie 02 03 2021.java
+++ Zadanie 02 03 2021/Zadanie 02 03 2021.java
@@ -4,15 +4,13 @@ public class Zadanie02032021 {
 
     }
     private static void Tablicadwuwymiarowa(){
-        int[][] tab2 = new int[4][4];
+        int[][] tab2 = new int[2][2];
         tab2[0][0] = 1;
         tab2[0][1] = 2;
-        tab2[0][2] = 3;
-        tab2[0][3] = 4;
-        tab2[1][0] = 1;
-        tab2[1][1] = 2;
-        tab2[1][2] = 3;
-        tab2[1][3] = 4;
+        tab2[1][0] = 3;
+        tab2[1][1] = 4;
+        
+		
 
         for(int i=0;i<tab2.length;i++){
             for(int j=0;j<tab2.length;j++){
