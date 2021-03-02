8e012ecf32d1077a1c519ba7641641b1bd7dbd78
NorbertFicner <77607718+NorbertFicner@users.noreply.github.com>
2021-03-02 10:31:27 +0100

    Dodnao tabliczke mnozenia

--- Zadanie 02 03 2021/Zadanie 02 03 2021.java
+++ Zadanie 02 03 2021/Zadanie 02 03 2021.java
@@ -1,6 +1,8 @@
+
+
 public class Zadanie02032021 {
     public static void main(String[]args){
-        Tablicadwuwymiarowa();
+        TabliczkaMnozenia();
 
     }
     private static void Tablicadwuwymiarowa(){
@@ -9,13 +11,29 @@ public class Zadanie02032021 {
         tab2[0][1] = 2;
         tab2[1][0] = 3;
         tab2[1][1] = 4;
-        
-		
+
+
 
         for(int i=0;i<tab2.length;i++){
             for(int j=0;j<tab2.length;j++){
                 System.out.println(tab2[i][j]);
             }
         }
+    }
+    private static void TabliczkaMnozenia(){
+    int [][] tab = new int[10][10];
+    System.out.println("Tabliczka mnozenia: ");
+        for(int i=0;i<tab.length;i++){
+            for(int j=0;j<tab.length;j++){
+                tab[i][j]= (i+1) * (j+1);
+            }
+        }
+        for(int i=0;i<tab.length;i++){
+            for(int j=0;j<tab.length;j++){
+                System.out.print(tab[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
     }
 }
