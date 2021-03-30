869561959e1fc395f2957e294a4a974c5e193613
NorbertFicner <77607718+NorbertFicner@users.noreply.github.com>
2021-03-30 09:17:30 +0200

    Zad 1

--- TworzenieObiektow/CwiczenieObiektowe_1.java
+++ TworzenieObiektow/CwiczenieObiektowe_1.java
@@ -1,4 +1,9 @@
 
 class CwiczenieObiektowe_1 {
+    public static void main(String[]args){
+        System.out.println("Zad1");
+        MojeDane md = new MojeDane("Norbert","Ficner","2PTN");
+        System.out.println("Zad2");
+    }
 
 }
---
+++ TworzenieObiektow/MojeDane.java
@@ -0,0 +1,10 @@
+ class MojeDane {
+    /*
+    public String imie = "Norbert";
+    public String nazwisko = "Ficner";
+    public String klasa = "2PTN";
+  */
+    public MojeDane(String imie,String nazwisko, String klasa){
+        System.out.println(imie + "\n" + nazwisko + "\n" + klasa);
+    }
+}
