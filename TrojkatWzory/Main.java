c14303a9e6d962ce778281ee27722d3d57513c0f
NorbertFicner <77607718+NorbertFicner@users.noreply.github.com>
2021-04-13 09:54:34 +0200

    4 wzory

--- TrojkatWzory/Main.java
+++ TrojkatWzory/Main.java
@@ -1,5 +1,8 @@
-public class Main {
+class Main {
     public static void main(String[]args){
-        
+        Triangle tr1 = new Triangle(5,7);
+        Triangle tr2 = new Triangle(5,7,2,6);
+        Triangle tr3 = new Triangle(4.0,6);
+        Triangle tr4 = new Triangle(5.0,10,6,3);
     }
 }
--- TrojkatWzory/Triangle.java
+++ TrojkatWzory/Triangle.java
@@ -1,3 +1,20 @@
-public class Triangle {
+import static java.lang.Math.*;
+
+class Triangle {
+
+    public Triangle(int wysokosc, int podstawa){
+        System.out.println(wysokosc * podstawa/2);
+    }
+    public Triangle(int a, int b,  int c, int r){
+        System.out.println(a * b * c / 4 * r);
+    }
+    public Triangle(double r, double p){
+        System.out.println(r * p);
+    }
+    public Triangle(double p ,int a ,int b, int c){
+        System.out.println(sqrt(p * (p-a) * (p-b) * (p-c)));
+    }
+
+
 
 }
