ea0011c4bb95c7bfe559db330528e10f240ba4ec
NorbertFicner <77607718+NorbertFicner@users.noreply.github.com>
2021-04-13 10:11:40 +0200

    5 wzor

--- TrojkatWzory/Main.java
+++ TrojkatWzory/Main.java
@@ -1,8 +1,11 @@
+import java.util.concurrent.TransferQueue;
+
 class Main {
     public static void main(String[]args){
         Triangle tr1 = new Triangle(5,7);
         Triangle tr2 = new Triangle(5,7,2,6);
         Triangle tr3 = new Triangle(4.0,6);
         Triangle tr4 = new Triangle(5.0,10,6,3);
+        Triangle tr5 = new Triangle(5);
     }
 }
--- TrojkatWzory/Triangle.java
+++ TrojkatWzory/Triangle.java
@@ -1,18 +1,25 @@
 import static java.lang.Math.*;
 
 class Triangle {
-
+    //Pole trojkata wysokosc i podstawa
     public Triangle(int wysokosc, int podstawa){
         System.out.println(wysokosc * podstawa/2);
     }
+    //Pole trojkata boki a , b , c i promien okregow
     public Triangle(int a, int b,  int c, int r){
         System.out.println(a * b * c / 4 * r);
     }
+    //Pole trojkata Obwod trojakta i promien okregow
     public Triangle(double r, double p){
         System.out.println(r * p);
     }
+    //Pole trojkata Obwod i dlugosci bokow
     public Triangle(double p ,int a ,int b, int c){
         System.out.println(sqrt(p * (p-a) * (p-b) * (p-c)));
+    }
+    //Trojakt rownoboczny pole dlugosc boku
+    public Triangle(double a){
+        System.out.println(pow(a,2) * sqrt(3) / 4);
     }
 
 
